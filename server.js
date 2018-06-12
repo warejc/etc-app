a74769a92636dd4fcc763c9ce3a5e6b3fc66be5e
Julius Ware <jcware8@gmail.com>
2018-06-12 16:16:31 -0400

    Change port

--- server.js
+++ server.js
@@ -4,5 +4,12 @@ const os = require('os');
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+/*app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+*/
+app.get('/', (req, res) => {
+    res.render('App');
+});
+
+app.listen(5000, () => console.log('Listening on port 5000!'));
+
+
