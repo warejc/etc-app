e1719811e096daaf36815b3c9610ba5e6aedead7
Julius Ware <jcware8@gmail.com>
2018-06-13 14:49:09 -0400

    Update run script

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const os = require('os');
-
+const path = require('path');
 const app = express();
 
 app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, 'public')));
 /*app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 */
 app.get('/', (req, res) => {
