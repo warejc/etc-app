67d4aba880b37a16874315e23713296ee71897b7
Julius Ware <jcware8@gmail.com>
2018-07-02 13:09:21 -0400

    Set up header

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.css';
-import {Button, Icon} from 'react-materialize'
+import {Button, Icon, Navbar, NavItem} from 'react-materialize'
 
 export default class App extends Component {
   constructor(props) {
@@ -14,11 +14,12 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
-        <Button waves='light'>
-          <Icon>thumb_up</Icon>
-       </Button>
-      </div>
+        <Navbar brand='ETC' className='black' right>
+          <NavItem>Photography</NavItem>
+          <NavItem>Content</NavItem>
+          <NavItem>Music</NavItem>
+        </Navbar>
+
     );
   }
 }
